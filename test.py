f8a5c3f16134c35c4774b3e777729774b3e0363e
AmirRuzdan <gh5322@myamu.ac.in>
2022-06-29 16:55:56 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+t = int(input)
+
+
+for i in range(t):
+
+    x,y= [int(x) for x in input().split()]
+    z = mod(x-y)
+    if x==y:
+        print("0")
+    elif x-y ==
+    p = max(a,b)
+    q = max(c,d)
+    if p<q:
+        print("P")
+    elif p==q:
+        print("TIE")
+    else:
+        print("Q")
