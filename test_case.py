fbe5820b7b587fda8080457e358f1229bbad6a8d
Amir Ruzdan <81528997+AmirRuzdan@users.noreply.github.com>
2022-06-29 19:28:17 +0530

    test runner

---
+++ test_case.py
@@ -0,0 +1,19 @@
+import random
+
+t = random.randint(1,5)
+x = random.randint(1,2000)
+y = random.randint(1,2000)
+
+
+print(t)
+print(x)
+print(y)
+
+# cook your dish here
+
+
+
+for i in range(t):
+
+    print(x+y)
+
